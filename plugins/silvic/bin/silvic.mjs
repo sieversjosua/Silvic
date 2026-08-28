b93d09c05d597fe940f87c140762b2f0245e8e56
Josua Sievers <36811422+sieversjosua@users.noreply.github.com>
2026-08-28 12:35:38 +0200

    release: v0.1.59 (#28)

--- plugins/silvic/bin/silvic.mjs
+++ plugins/silvic/bin/silvic.mjs
@@ -29627,7 +29627,7 @@ var codexEnvironmentRequestSchema = external_exports.object({
 // package.json
 var package_default = {
   name: "@silvic/cli",
-  version: "0.1.58",
+  version: "0.1.59",
   description: "Non-interactive runtime and preview control for Silvic",
   repository: "https://github.com/sieversjosua/Silvic",
   license: "MIT",
